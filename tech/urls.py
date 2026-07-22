018ba00ffb2db5ede7aeb2447e9afacd5ddb536e
smackotacrazy0104 <ismael_gtslvl@hotmail.com>
2026-07-22 10:12:11 -0300

    aula4

--- tech/urls.py
+++ tech/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from tech.views import index
+from tech.views import index, members
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('members/', members, name='members')
 ]
